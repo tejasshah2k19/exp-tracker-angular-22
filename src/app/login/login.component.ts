bdaeb98a8a0d8655b5c468685bc72c27828af53c
tejas <tejasshah@gmail.com>
2022-07-20 20:57:33 +0530

    validation and router

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,11 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  email:string =""
+  password:string=""
   constructor() { }
 
   ngOnInit(): void {
     console.log("Login....component....");
+  }
+
+  login()
+  {
+    console.log(this.email);
+    console.log(this.password);
+    
   }
 
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,18 +13,53 @@ export class SignupComponent implements OnInit {
   email:string="" //instance variable 
   password:string=""
 
-  constructor() { }
+  firstNameError = ""
+  emailError = "" 
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
     console.log("Signup....component....");    
   }
 
-  printData(){
+  signup(){
 
     console.log(this.email);//this -> email 
     console.log(this.firstName);
     console.log(this.lastName);
     console.log(this.password);
     
+
+    //validation - required 
+    let isError  = false 
+    if(this.firstName == "" || this.firstName.trim().length == 0 ){
+      isError = true; 
+      this.firstNameError  = "Please Enter FirstName"
+    }else{
+      this.firstNameError = ""
+    }
+
+    //XX@XX.XXX
+    if(this.email == "" || this.email.trim().length == 0 ){
+      isError = true;
+      this.emailError = "Please Enter Email"
+    }else{
+      this.emailError = ""
+    }
+    
+    if(isError == true ){
+      console.log("Error ");
+      
+    }else{
+      //login   
+      this.router.navigateByUrl("/login");
+
+    }
+    //validation - format 
+    //email duplicate
+
+    //db save 
+
+    //redirect login 
   }
 }
